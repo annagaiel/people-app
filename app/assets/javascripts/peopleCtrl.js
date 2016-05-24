292463e1d18eb097542ee1b0583f0bcc3a34a276
Anna Natorilla <annagaiel@gmail.com>
2016-05-23 21:29:55 -0700

    Finish delete show and form

--- app/assets/javascripts/peopleCtrl.js
+++ app/assets/javascripts/peopleCtrl.js
@@ -3,6 +3,40 @@
   angular.module("app").controller("peopleCtrl",
     function($scope){
 
+      $scope.people = [
+        {
+          name: "Anna",
+          bio: "Wonderful person",
+          bioVisible: false
+        },
+        {
+          name: "Mary",
+          bio: "Graceful person",
+          bioVisible: false
+        },
+        {
+          name: "Sally",
+          bio: "Funny person",
+          bioVisible: false
+        }
+      ]
+
+      $scope.toggleBio = function(person){
+        person.bioVisible = !person.bioVisible
+      }
+
+      $scope.addPerson = function(newName, newBio){
+        if (newName && newBio) {
+          $scope.people.push({name:newName, bio:newBio, bioVisible: false})
+          $scope.newName = null;
+          $scope.newBio = null;
+        }
+      }
+
+      $scope.deleteMe = function(index){
+        $scope.people.splice(index, 1);
+      }
+
       window.$scope = $scope;
   });
 })();
